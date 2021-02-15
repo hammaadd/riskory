9330a1144dcecaa07108aca2079b505091681646
Shoaib Latif <shoaib@virtuenetz.com>
2021-02-15 17:49:12 +0500

    Classes Updated

--- resources/views/visitor/categories/index.blade.php
+++ resources/views/visitor/categories/index.blade.php
@@ -14,14 +14,14 @@
         <div class="pl-0 col-12 col-md-12 py-5 pr-0 pr-md-3 pr-lg-5">
           
             <!-- Browse By Industry Secion -->
-            <div class="pl-0 row mb-5">
-                <div class="col-sm-auto col-12">
-                    <span class="bg-lblue font-eb font-18 py-3 px-2 px-md-5 rounded-right-xl shadow-sm"><img src="assets/images/Mask-Group-55.svg" class="align-bottom" width="35px">&nbsp;&nbsp;Browse By Industry</span>
+            <div class="row mb-5 mx-0 align-items-sm-center">
+                <div class="col-sm-auto col-12 pl-0">
+                    <p class="bg-lblue font-eb font-18 py-2 px-5 mb-0 rounded-right-xl shadow-sm"><img src="assets/images/Mask-Group-55.svg" class="align-bottom" width="35px">&nbsp;&nbsp;Browse By Industry</p>
                 </div>
                 <div class="col-sm-auto col-12 mt-3 mt-sm-0 text-muted">
                     <small><i class="fas fa-info-circle pr-2"></i><span >Based on <a target="_blank" href="https://en.wikipedia.org/wiki/Global_Industry_Classification_Standard">Global Industry Classification Standard</a></span></small>
                 </div>
-                <div class="col-sm-auto col-12 mt-3 mt-sm-0 ml-auto">
+                <div class="col-md-auto col-12 mt-3 mt-md-0 ml-auto">
                     <button class="btn bg-red text-light br-7 font-12 text-capitalize font-weight-bold" title="View All Risk Controls" onclick="return parent.location='{{route('rc.all.public')}}';">All risk controls</button>
                 </div>
             </div>
@@ -30,20 +30,19 @@
                     <div class="col-12 col-sm-6 col-md-4 px-4 mb-3">
                         <div class="div-hover">
                             <p class="p-style mb-0 d-inline mr-3"><a href="{{route('byControlPublic',['control'=>$ind,'type'=>$ind->type])}}">{{$ind->name}}</a> ({{$ind->rccontrols->whereNotIn('rc.status',['P','R'])->count()}})</p>
-                           
                         </div>
                     </div>
                 @endforeach
             </div>
 
 
-            <div class="ml-5 my-2">
+            <div class="ml-3 ml-md-5 my-2">
                 <a href="{{route('seeMore','industries')}}" class="btn bg-red text-white br-7 font-16 text-capitalize font-weight-bold">More Industries</a>
             </div>
 
-            <div class="pl-0 row my-5">
-                <div class="col-sm-auto col-12">
-                    <span class="bg-lblue font-eb font-18 py-3 px-2 px-md-5 rounded-right-xl shadow-sm"><img src="assets/images/Mask Group 56.svg" class="align-bottom" width="35px">&nbsp;&nbsp;Browse By Business Processes</span>
+            <div class="row mb-5 mx-0 my-5 align-items-sm-center">
+                <div class="col-sm-auto col-12 pl-0">
+                    <p class="bg-lblue font-eb font-18 py-2 px-5 mb-0 rounded-right-xl shadow-sm"><img src="assets/images/Mask Group 56.svg" class="align-bottom" width="35px">&nbsp;&nbsp;Browse By Business Processes</p>
                 </div>
                 <div class="col-sm-auto col-12 mt-3 mt-sm-0 text-muted">
                     <small><i class="fas fa-info-circle pr-2"></i><span >Based on <a target="_blank" href="https://www.apqc.org/resource-library/resource-listing/apqc-process-classification-framework-pcf-cross-industry-excel-7">APQC Process Classification Framework (PCF)</a></span></small>
@@ -59,13 +58,13 @@
                     </div>
                 @endforeach
             </div>
-            <div class="ml-5 my-2">
+            <div class="ml-3 ml-md-5 my-2">
                 <a href="{{route('seeMore','bprocesses')}}" class="btn bg-red text-white br-7 font-16 text-capitalize font-weight-bold">More Business Processes</a>
             </div>
             <!-- Browse By Framework Section -->
-            <div class="pl-0 row my-5 ">
-                <div class="col-sm-auto col-12">
-                    <span class="bg-lblue font-eb font-18 py-3 px-2 px-md-5 rounded-right-xl shadow-sm"><img src="assets/images/Mask Group 57.svg" class="align-bottom" width="35px">&nbsp;&nbsp;Browse By Frameworks</span>
+            <div class="row mb-5 mx-0 my-5 align-items-sm-center">
+                <div class="col-sm-auto col-12 pl-0">
+                    <p class="bg-lblue font-eb font-18 py-2 px-5 mb-0 rounded-right-xl shadow-sm"><img src="assets/images/Mask Group 57.svg" class="align-bottom" width="35px">&nbsp;&nbsp;Browse By Frameworks</p>
                 </div>
                 <div class="col-sm-auto col-12 mt-3 mt-sm-0 text-muted">
                     {{-- <small class="mt-5"><i class="fas fa-info-circle pr-2"></i><span >So this is disclaimer here</span></small> --}}
@@ -81,7 +80,7 @@
                     </div>
                 @endforeach
             </div>
-            <div class="ml-5 my-2">
+            <div class="ml-3 ml-md-5 my-2">
                 <a href="{{route('seeMore','bframeworks')}}" class="btn bg-red text-white br-7 font-16 text-capitalize font-weight-bold">More Business Frameworks</a>
             </div>
 

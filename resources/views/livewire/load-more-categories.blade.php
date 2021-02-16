0b26b6a26c3f64f33a6ffd804ab6b4a3e0a3d131
Shoaib Latif <shoaib@virtuenetz.com>
2021-02-16 15:30:07 +0500

    Classes and HTML updated

--- resources/views/livewire/load-more-categories.blade.php
+++ resources/views/livewire/load-more-categories.blade.php
@@ -1,5 +1,3 @@
 <div>
-    
     <button wire:click="loadMore" class="btn bg-red text-white br-7 font-16 text-capitalize font-weight-bold float-right">Load More <i class="fa fas-arrow-down"></i></button>
-    
 </div>

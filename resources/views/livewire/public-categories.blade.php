9010889edae51f0bd2199d2aaddc084c8baa4717
Shoaib Latif <shoaib@virtuenetz.com>
2021-02-16 19:15:36 +0500

    HTML updated

--- resources/views/livewire/public-categories.blade.php
+++ resources/views/livewire/public-categories.blade.php
@@ -2,11 +2,9 @@
     <div class="col-12">
         <div class="row pl-3 pl-md-5 pt-3">
             @foreach($data as $dat)
-                <div class="col-12 col-sm-6 col-lg-4 px-3 mb-3">
+                <div class="col-12 col-sm-6 col-lg-4 mb-3">
                     <div class="div-hover">
                         <p class="p-style mb-0 d-inline mr-3"><a href="{{route('byControlPublic',['control'=>$dat,'type'=>$dat->type])}}">{{$dat->name}}</a> ({{$dat->rccontrols->whereNotIn('rc.status',['P','R'])->count()}})</p>
-
-                       
                     </div>
                 </div>
             @endforeach
--- resources/views/visitor/categories/viewByControl.blade.php
+++ resources/views/visitor/categories/viewByControl.blade.php
@@ -1,62 +1,56 @@
 @extends('user.layout.visitor')
 @section('SiteTitle','Risk controls || Riskory')
 @section('content')
-<style>
-    html,body {
-        min-height: 100%;
-        height: 100%;
-    }
-    .cover-container {
-        height: 100%;
-    }
-    </style>
     <main role="main" class="py-2">
-        <div class="px-0 col-12 col-md-12 py-2">
-            <div class="row pt-4 mx-0">
-                <div class="pl-0 col-12 col-sm-auto">
-                    
-                    <p class="bg-lblue font-eb font-18 py-2 px-5 rounded-right-xl shadow-sm"><i><img src="{{asset('assets/images/Mask Group 10@2x.png')}}" class="align-bottom" width="35px"></i>&nbsp;&nbsp;
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12 py-5">
+                    <div class="sect--title pl-3 pl-md-5">
+                        <div class="row mb-4 align-items-md-center">
+                            <div class="col-md-auto col-12 px-0 pr-md-3 sect--title__col">
+                                <p class="bg-lblue font-eb d-sm-inline-block font-18 py-2 pl-4 pl-md-5 pr-5 mb-0 rounded-right-xl shadow-sm"><img src="{{asset('assets/images/Mask Group 10@2x.png')}}" class="align-bottom" width="35px">&nbsp;&nbsp;
+                                @if(isset($control))
+                                    {{$control->name}}
+                                @endif
+                                @if(isset($tag))
+                                    {{$tag->name}}
+                                @endif
+                                    Risk Controls</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="container">
+                        <div class="row">
+                            @if(isset($control))
+                                @livewire('by-control-public',['control'=>$control,'page'=>1,'perPage'=>6])  
+                            @endif 
+                            {{-- @if(isset($tag))
+                                @livewire('by-tag',['tag'=>$tag,'page'=>1,'perPage'=>6])  
+                            @endif  --}}
+                        </div>
+
+
+                        <div class="row">
+                             {{-- Risk control starts here --}}
                         @if(isset($control))
-                            {{$control->name}}
+                            @if($data->count()<1)
+                                <h6 class="lead">No riskcontrols found for "{{$control->name}}" </h6>
+                            @endif
                         @endif
                         @if(isset($tag))
-                            {{$tag->name}}
+                            @if($data->count()<1)
+                                <h6 class="lead">No riskcontrols found for "{{$tag->name}}" </h6>
+                            @endif
                         @endif
-                            Risk Controls</p>
-                </div>
-           
-                <div class="col-lg-3 col-md-6 col-sm-12 col-12 ml-md-auto">
-                    <div class="topbar-icon text-center text-md-right mt-3 mt-md-0">
-                        {{-- @include('user.sections.filters') --}}
+                         {{-- Risk control starts here --}}
+                         @include('visitor.sections.byControlRcs')
+                         {{-- Riskcontrol ends here --}}
+                    
+                            {{-- Riskcontrol ends here --}}
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="row pt-4 mx-2">
-                @if(isset($control))
-                    @livewire('by-control-public',['control'=>$control,'page'=>1,'perPage'=>6])  
-                @endif 
-                {{-- @if(isset($tag))
-                    @livewire('by-tag',['tag'=>$tag,'page'=>1,'perPage'=>6])  
-                @endif  --}}
-            </div>
-            
-            <div class="row px-2 px-md-5 mx-0 mx-md-5 pt-5">
-                 {{-- Risk control starts here --}}
-            @if(isset($control))
-                @if($data->count()<1)
-                    <h6 class="lead">No riskcontrols found for "{{$control->name}}" </h6>
-                @endif
-            @endif
-            @if(isset($tag))
-                @if($data->count()<1)
-                    <h6 class="lead">No riskcontrols found for "{{$tag->name}}" </h6>
-                @endif
-            @endif
-             {{-- Risk control starts here --}}
-             @include('visitor.sections.byControlRcs')
-             {{-- Riskcontrol ends here --}}
-        
-                {{-- Riskcontrol ends here --}}
             </div>
         </div>
         {{-- @include('user.sections.ratingModal') --}}

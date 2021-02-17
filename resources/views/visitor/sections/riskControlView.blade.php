86cb8860469852a57c316674701f8edadb0ae2b3
Hammad Ur Rafay <hammad.virtuenetz@gmail.com>
2021-02-17 12:47:14 +0500

    Changes in riskcontrol

--- resources/views/visitor/sections/riskControlView.blade.php
+++ resources/views/visitor/sections/riskControlView.blade.php
@@ -63,9 +63,9 @@
                                 </a>
 
                                 <ul class="rc--share-menu">
-                                    <li><a href="https://www.facebook.com/sharer/sharer.php?display=page&u={{route('rc.view',$rc->id)}}" target="_blank" class="box-shadow"><i class="fab fa-facebook-f"></i></a></li>
-                                    <li><a href="https://twitter.com/intent/tweet?text={{$rc->title}}&amp;url={{route('rc.view',$rc->id)}}" target="_blank" class="box-shadow"><i class="fab fa-twitter"></i></a></li>
-                                    <li><a href="https://wa.me/?text={{route('rc.view',$rc->id)}}" target="_blank" class="box-shadow"><i class="fab fa-whatsapp"></i></a></li>
+                                    <li><a href="https://www.facebook.com/sharer/sharer.php?display=page&u={{route('rc.view',$rc->id)}}" target="_blank" class="box-shadow" title="Share on facebook"><i class="fab fa-facebook-f"></i></a></li>
+                                    <li><a href="https://twitter.com/intent/tweet?text={{$rc->title}}&amp;url={{route('rc.view',$rc->id)}}" target="_blank" class="box-shadow" title="Share on twitter"><i class="fab fa-twitter"></i></a></li>
+                                    <li><a href="https://wa.me/?text={{route('rc.view',$rc->id)}}" target="_blank" class="box-shadow" title="Share on whatsapp"><i class="fab fa-whatsapp" ></i></a></li>
                                 </ul>
                             </div>
                         </div>

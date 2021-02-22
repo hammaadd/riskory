98b9437216cabb9fafc76bc71123d57aa41a999a
Hammad Ur Rafay <hammad.virtuenetz@gmail.com>
2021-02-22 21:32:39 +0500

    Categories View Hierarchy

--- resources/views/user/contributor/viewMore.blade.php
+++ resources/views/user/contributor/viewMore.blade.php
@@ -12,5 +12,23 @@
     </div>
 
     @livewire('categories',['data'=>$data,'req'=>$req,'page'=>1,'perPage'=>16])
+
+    {{-- <div class="row">
+        
+            <div class="col-12 col-sm-6 col-lg-6 px-3 mb-2">
+                <div class="div-hover">
+                    <p class="p-style mb-0 d-inline mr-3"><a href="#collapseExample" data-toggle="collapse"><i class="fas fa-caret-down"></i>  </a><a href="#" data-toggle="tooltip" title="">Checking the category</a></p>
+
+                   
+                        <button class="btn-follow btn-follow-2">Follow</button>
+                   
+                  
+                </div>
+                <div class="collapse" id="collapseExample">
+
+                </div>
+            </div>
+           
+        </div> --}}
 </div>
 @endsection

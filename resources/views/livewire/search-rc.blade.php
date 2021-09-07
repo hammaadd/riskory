c492602a2fedaf3eaa5d155087990ed98b89643b
Hammad Ur Rafay <hammad.virtuenetz@gmail.com>
2021-09-07 12:35:18 +0500

    Tags added on search results frontend

--- resources/views/livewire/search-rc.blade.php
+++ resources/views/livewire/search-rc.blade.php
@@ -1,19 +1,22 @@
 <div class="col-lg-4 pt-4 pt-lg-2 text-center position-relative ">
-  <form action="{{route('search.rcs')}}" method="GET" autocomplete="off">
+  <form action="{{route('advance.search.results')}}" method="GET" autocomplete="off">
     <input autocomplete="false" name="hidden" type="text" style="display:none;">
     <div class="input-group search-bar">
 
-
-      <input type="text" wire:model.debounce.400ms="search" name="search" class="form-control" placeholder="Search Here" aria-label="Search Here" aria-describedby="basic-addon2">
+      <input type="text" name="search" class="form-control" placeholder="Search Here" aria-label="Search Here" aria-describedby="basic-addon2">
+      {{-- <input type="text" wire:model.debounce.400ms="search" name="search" class="form-control" placeholder="Search Here" aria-label="Search Here" aria-describedby="basic-addon2"> --}}
       <div class="input-group-append">
         <button class="btn-search" type="submit"><i class="fas fa-search"></i></button>
       </div>
 
     </div>
+    <div>
+    <a href="{{route('advanced.search')}}" class="float-right d-inline-block"><span class="color-r">Advanced Search <i class="fas fa-cog"></i></span></a>
+    </div>
     {{-- <a href="{{route('advanced.search')}}" class="float-right"><small class="color-r">Advanced Search <i class="fas fa-cog"></i></small></a> --}}
   </form>
-
-<div class="row position-absolute search-dropdown style-2 scrollbar shadow br-7" id="search---dropdown">
+{{-- </div> --}}
+{{-- <div class="row position-absolute search-dropdown style-2 scrollbar shadow br-7" id="search---dropdown">
     @if(isset($rcs) || isset($controls))
     <div class="col-12 mt-2 ">
         @if(isset($rcs))
@@ -37,27 +40,35 @@
        {{$rc->title}}
       </a>
 
-      {{-- <a href="#" class="list-group-item list-group-item-action"></a> --}}
       @endforeach
     @endif
     @endif
 
   </div>
-  @if(isset($controls))
-
+  @if(isset($tags))
   <div class="mx-1 mt-2 w-100">
-    @if($controls->count() > 0)
-        <p class="font-18 font-bold">Categories</p>
-        @foreach($controls as $cont)
-        <button class="btn bg-white color-r box-shadow br-7 font-14 px-2 text-capitalize mb-2 btn-hover" onclick="return parent.location='{{route('byControl',['control'=>$cont,'type'=>$cont->type])}}'">{{$cont->name}}</button>
+    @if($tags->count() > 0)
+        <p class="font-18 font-bold">Tags</p>
+        @foreach($tags as $tg)
+        <button class="btn bg-white color-r box-shadow br-7 font-14 px-2 text-capitalize mb-2 btn-hover" onclick="return parent.location='{{route('byTag',['tag'=>$tg])}}'" title="{{$tg->name}}">{{Str::limit($tg->name,30)}}</button>
         @endforeach
-    @elseif(!($controls->count() > 0) && !($rcs->count() > 0))
-        <p  class="font-18 font-bold">No results to show for "{{$search}}"</p>
+    @elseif(!($tags->count() > 0) && !($rcs->count() > 0))
+        
     @endif
   </div>
-
-
+  @endif
+  @if(isset($controls))
+    <div class="mx-1 mt-2 w-100">
+      @if($controls->count() > 0)
+          <p class="font-18 font-bold">Categories</p>
+          @foreach($controls as $cont)
+          <button class="btn bg-white color-r box-shadow br-7 font-14 px-2 text-capitalize mb-2 btn-hover" onclick="return parent.location='{{route('byControl',['control'=>$cont,'type'=>$cont->type])}}'" title="{{$cont->name}}">{{Str::limit($cont->name,30)}}</button>
+          @endforeach
+      @elseif(!($controls->count() > 0) && !($rcs->count() > 0))
+          <p  class="font-18 font-bold">No results to show for "{{$search}}"</p>
+      @endif
+    </div>
   @endif
 
-</div>
+</div> --}}
 
